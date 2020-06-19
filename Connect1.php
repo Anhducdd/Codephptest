174f0f5a03dfbb844565095730b42800e2b4b11e
Anh Duc(andy) <53077402+Anhducdd@users.noreply.github.com>
2020-06-19 23:57:18 +0700

    Update Connect1.php

--- Connect1.php
+++ Connect1.php
@@ -1,5 +1,8 @@
 <?php
 $errors = array();
+$conn_string="host=ec2-34-230-231-71.compute-1.amazonaws.com
+ port=5432 dbname=d5mr838qoopsco
+ user=iplpufvrvvxxst password=ef79ce7118c05ebef0d9b63b469445a04b44aaa62c1964d2a02bec5c31ea93e5";
 $dbconn= pg_connect($conn_string);
 if (isset($_POST['username'])) {
 	# code...
