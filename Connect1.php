0ade05b233f0d9e61681ecd2b980bf39371b809b
Anh Duc(andy) <53077402+Anhducdd@users.noreply.github.com>
2020-06-19 23:56:40 +0700

    Create Connect1.php

---
+++ Connect1.php
@@ -0,0 +1,24 @@
+<?php
+$errors = array();
+$dbconn= pg_connect($conn_string);
+if (isset($_POST['username'])) {
+	# code...
+	$username = $_POST['username'];
+}
+
+if (isset($_POST['pass'])) {
+	# code...
+	$pass = $_POST['pass'];
+}
+$sql = "SELECT * FROM tblAccount WHERE _user='".$username."' AND _pass='".$pass."'";
+$result =pg_query($dbconn, $sql);
+$row = pg_num_rows($result);
+if ($row==1)
+{
+	echo "Login success";
+}
+else
+{
+	array_push($errors, "Wrong username/password combination");
+}
+?>
